68d7ae1b39346f6efdcaa946c3ee0324f014bde1
artvsantos <arthursantos334@gmail.com>
2024-04-25 15:52:30 -0300

    25/04/2024

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+# first_name(string), last_name(string), phone(string),
+# email(email), created_date(date), description(text),
+# category(foreign key), show(boolean), owner(foreign key),
+# picture(image)
+
 # Create your models here.
