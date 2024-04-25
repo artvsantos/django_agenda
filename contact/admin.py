5fa88bc36add3a2faf102faa2bd6f2fb8168f7b4
artvsantos <arthursantos334@gmail.com>
2024-04-25 17:04:35 -0300

    25/04/2024

--- contact/admin.py
+++ contact/admin.py
@@ -6,4 +6,11 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('id', 'first_name', 'last_name', 'phone')
+    ordering = ('id', 'first_name', 'phone')
+    search_fields = ('id', 'first_name', 'last_name')
+    list_per_page = 10
+    list_max_show_all = 50
+    list_display_links = 'id', 'first_name', 'last_name',
+    # list_editable = 'last_name'
+    # list_filter = ('created_date',)
