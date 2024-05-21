9312b202d6232c535bb862b167682124547ba4b9
artvsantos <arthursantos334@gmail.com>
2024-05-21 18:14:54 -0300

    21/05/2024

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,16 +1,33 @@
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
+from django import forms
 
 from contact.models import Contact
 
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )
+
+
 def create(request):
     if request.method == 'POST':
-        search = request.POST.get('first_name')
+        context = {
+            'form': ContactForm(data=request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
 
     context = {
-
+        'form': ContactForm()
     }
 
     return render(
