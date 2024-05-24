56070a833c86caec5793a59e316581be19011d00
artvsantos <arthursantos334@gmail.com>
2024-05-24 12:30:50 -0300

    24/05/2024

--- contact/forms.py
+++ contact/forms.py
@@ -5,11 +5,38 @@ from . import models
 
 
 class ContactForm(forms.ModelForm):
+    last_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Escreva aqui',
+            }
+        ),
+        label='Ultimo nome',
+        help_text='Texto de ajuda',
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['first_name'].widget.attrs.update({
+            'class': 'classe-a classe-b',
+            'placeholder': 'Escreva aqui',
+        })
+
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
         )
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'class': 'classe-a classe-b',
+        #             'placeholder': 'Escreva aqui',
+        #         }
+        #     )
+        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
